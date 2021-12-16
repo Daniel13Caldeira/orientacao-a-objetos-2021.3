eae24e28edb8c8118e1362685743c87188da97f2
“Daniel13Caldeira” <daniel.caldeira@ice.ufjf.br>
2021-12-16 16:58:27 -0300

    Adicionando tela inicial do funcionário

---
+++ made_lavant/src/main/java/com/mycompany/made_lavant/Made_Lavant.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.made_lavant;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Made_Lavant {
+    
+}
